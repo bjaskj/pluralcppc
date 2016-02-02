81a0c694787eddd2aa7e64cadf0a4f203fa69793
Bjarte Skjørestad <bjarte@timeanddate.com>
2016-02-02 08:39:58 +0100

    Open-close principle.

--- design_patters_cpp/m1/main.cpp
+++ design_patters_cpp/m1/main.cpp
@@ -28,7 +28,146 @@ struct PersistenceManager {
     }
 };
 
+// Open-closed principle
+
+enum class Color {Red, Green, Blue};
+enum class Size {Small, Medium, Large};
+
+struct Product {
+    string name;
+    Color color;
+    Size size;
+};
+
+// bad filter
+
+struct ProductFilter {
+    typedef vector<Product*> Items;
+
+    static Items by_color(Items items, Color color) {
+        Items results;
+        for (auto &i : items) {
+            if (i->color == color) {
+                results.push_back(i);
+            }
+        }
+
+        return results;
+    }
+
+    static Items by_size(Items items, Size size) {
+        Items results;
+        for (auto &i : items) {
+            if (i->size == size) {
+                results.push_back(i);
+            }
+        }
+
+        return results;
+    }
+
+    static Items by_color_and_size(Items items, Color color, Size size) {
+        Items results;
+        for (auto &i : items) {
+            if (i->size == size && i->color == color) {
+                results.push_back(i);
+            }
+        }
+
+        return results;
+    }
+};
+
+// Better filter
+
+template <typename T> struct ISpecification {
+    virtual bool is_satisfied(T* item) = 0;
+};
+
+template <typename T> struct IFilter {
+    virtual vector<T*> filter(vector<T*> items, ISpecification<T> &spec) = 0;
+};
+
+struct BetterFilter : IFilter<Product> {
+    typedef vector<Product*> Items;
+
+    virtual Items filter(Items items, ISpecification<Product> &spec) override {
+        Items result;
+        for (auto &p : items) {
+            if (spec.is_satisfied(p)) {
+                result.push_back(p);
+            }
+        }
+
+        return result;
+    }
+};
+
+struct ColorSpecification : ISpecification<Product> {
+    Color color;
+
+    explicit ColorSpecification(const Color color)
+            : color{color} {
+    }
+
+    bool is_satisfied(Product *item) override {
+        return item->color == color;
+    }
+};
+
+struct SizeSpecification : ISpecification<Product> {
+    Size size;
+
+    explicit SizeSpecification(const Size size)
+            : size{size} {
+    }
+
+    bool is_satisfied(Product *item) override {
+        return item->size == size;
+    }
+};
+
+template <typename T> struct AndSpecification : ISpecification<T> {
+    ISpecification<T> &first;
+    ISpecification<T> &second;
+
+    AndSpecification(ISpecification<T> &first, ISpecification<T> &second)
+            : first(first), second(second) {
+
+    }
+
+    bool is_satisfied(T *item) override {
+        return first.is_satisfied(item) && second.is_satisfied(item);
+    }
+};
+
+
+void openClosedPrinciple() {
+    Product apple{"Apple", Color::Green, Size::Small};
+    Product tree{"Tree", Color::Green, Size::Large};
+    Product house{"House", Color::Blue, Size::Large};
+
+    vector<Product*> all{&apple, &tree, &house};
+
+    BetterFilter bf;
+    ColorSpecification green_spec(Color::Green);
+
+    auto green_things = bf.filter(all, green_spec);
+    for (auto &x : green_things) {
+        cout << x->name << " is green." << endl;
+    }
+
+    SizeSpecification big_spec(Size::Large);
+    AndSpecification<Product> green_and_big{ big_spec, green_spec };
+
+    auto green_big_things = bf.filter(all, green_and_big);
+    for (auto &x : green_big_things) {
+        cout << x->name << " is green and large." << endl;
+    }
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    openClosedPrinciple();
     return 0;
 }
+
